635d9ba8cc4df6cc385e2ae43ef7b2cb64f44428
Elsai Deribu <elsaideribu7@gmail.com>
2023-04-19 13:17:05 +0300

    End of section 4

---
+++ Reactivities/Application/Activities/Edit.cs
@@ -0,0 +1,48 @@
+using MediatR;
+using Persistence;
+using Domain;
+using AutoMapper;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+
+        public class Command : IRequest
+        {
+            public Activity Activitiy { get; set; }
+        }
+
+
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+
+            {
+
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activitiy.Id);
+
+                _mapper.Map(request.Activitiy, activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+
+
+            }
+        }
+
+
+
+    }
+}
