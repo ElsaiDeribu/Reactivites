1d329422995111a39522761374b2573995bd8771
Elsai Deribu <elsaideribu7@gmail.com>
2023-04-28 11:47:09 +0300

    End of section 6

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -27,11 +28,17 @@ export default function ActivityDashboard({
   openForm,
   closeForm,
   deleteActivity,
+  submitting,
 }: props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity = {deleteActivity}/>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting}
+        />
       </Grid.Column>
       <GridColumn width="6">
         {selectedActivity && !editMode && (
@@ -46,6 +53,7 @@ export default function ActivityDashboard({
             closeForm={closeForm}
             activity={selectedActivity}
             createOrEdit={createOrEdit}
+            submitting = {submitting}
           />
         )}
       </GridColumn>
